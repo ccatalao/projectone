15e4865d1631df1b7f07979523b1576e2c088187
Carlos Alves <carlosalves@Carloss-MacBook-Pro-2.local>
2024-07-21 14:12:17 +0100

    changed database and user for postgrest

--- projectone/settings_prod.py
+++ projectone/settings_prod.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['167.71.34.9']
 
@@ -85,11 +85,8 @@ WSGI_APPLICATION = 'projectone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'OPTIONS': {
-                'options': '-c search_path=myschema'
-            },
-        'NAME': 'projectone',
-        'USER': 'projectoneuser',
+        'NAME': 'projectdb',
+        'USER': 'projectoneuserdb',
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': 'localhost',
         'PORT': ''
@@ -131,11 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#MEDIA_URL = 'media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 
